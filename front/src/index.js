6d7a6611e42d69a0d318b8329148b90499d7b53b
Goofy <573888699@qq.com>
2023-06-19 11:23:37 +0800

    front init

---
+++ front/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Chat from './components/Chat';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        // errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/chat",
+                element: <Chat />,
+            },
+        ],
+    },
+];
+const router = createBrowserRouter(routes);
+
+root.render(
+    <RouterProvider router={router} />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
