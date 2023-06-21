678f1cf58f99400061962202bb675a522d093c7d
Chao Chen <chaochen83@users.noreply.github.com>
2023-06-21 15:05:56 +0800

    fixing cors error

--- starknet_beego/main.go
+++ starknet_beego/main.go
@@ -14,11 +14,16 @@ func main() {
 	// - Origin header
 	// - Credentials share
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		// AllowOrigins:     []string{"https://*.foo.com"},
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost"},
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
+		//允许访问所有源
+		AllowAllOrigins: true,
+		//可选参数"GET", "POST", "PUT", "DELETE", "OPTIONS" (*为所有)
+		//其中Options跨域复杂请求预检
+		AllowMethods: []string{"*"},
+		//指的是允许的Header的种类
+		AllowHeaders: []string{"content-type"}, // Chao - can't be * here, otherwise it won't work!!
+		//公开的HTTP标头列表
+		ExposeHeaders: []string{"Content-Length"},
+		//如果设置，则允许共享身份验证凭据，例如cookie
 		AllowCredentials: true,
 	}))
 
